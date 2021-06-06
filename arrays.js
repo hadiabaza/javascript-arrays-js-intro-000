dd6abe05cbcfaa1ecea7481f27dbe37c5e134eaf
Hadi Abaza <hadiabaza1@gmail.com>
2021-06-06 17:10:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
+  let newArr = [element, ...array]
+  return newArr;
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element){
