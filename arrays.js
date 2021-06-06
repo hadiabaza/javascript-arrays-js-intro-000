868cfa45e76decb1c41375046e4df82c09edb1c2
Hadi Abaza <hadiabaza1@gmail.com>
2021-06-06 16:54:29 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var newArr = [element, ...array];
+  return newArr;
+};
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+};
+
+function addElementToEndOfArray(array, element){
+  return [array,...element];
+};
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return [array, ...element];
+}
